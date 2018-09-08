8450a8cdadc3e3c3adccfb095c73b1b7597284a1
aarogacki <alainarogacki@gmail.com>
2018-09-08 15:43:17 -0400

    added dataset

---
+++ GoodEatins/Model/FoodCategory.swift
@@ -0,0 +1,14 @@
+//
+//  FoodCategory.swift
+//  GoodEatins
+//
+//  Created by Alaina Rogacki on 9/8/18.
+//  Copyright © 2018 Alaina Rogacki. All rights reserved.
+//
+
+import Foundation
+
+struct FoodCategory {
+    let title: String
+    let imageName: String
+}
---
+++ GoodEatins/Model/Recipe.swift
@@ -0,0 +1,15 @@
+//
+//  Recipe.swift
+//  GoodEatins
+//
+//  Created by Alaina Rogacki on 9/8/18.
+//  Copyright © 2018 Alaina Rogacki. All rights reserved.
+//
+
+import Foundation
+
+struct Recipe {
+    var title: String
+    var instructions: String
+    var imageName: String
+}
