3a14ee76c89c831a5d9b81dec9fff51958979b00
aarogacki <alainarogacki@gmail.com>
2018-09-08 15:22:08 -0400

    init commit

---
+++ GoodEatins/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoodEatins
+//
+//  Created by Alaina Rogacki on 9/8/18.
+//  Copyright © 2018 Alaina Rogacki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
